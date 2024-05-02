537304f78e8657696514480e9fcb9a423652d38f
Bernd Storath <bernd.storath@offizium.de>
2024-05-02 12:30:42 +0200

    new problem

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,68 +1,85 @@
 <template>
   <Page>
-    <GridLayout height="100%" rows="auto, 125" columns="42, *, 42" backgroundColor="#F2F2F2">
+    <GridLayout height="100%" rows="125, 125" columns="42, *, 42" backgroundColor="#F2F2F2">
       <BarChart row="1" col="1" ref="chart" @loaded="onChartLoaded" height="100%" width="100%" />
     </GridLayout>
   </Page>
 </template>
 
 <script lang="ts">
-import { Canvas, Paint, createRect } from "@nativescript-community/ui-canvas";
+import { Paint, createRect } from "@nativescript-community/ui-canvas";
 import { BarChart } from "@nativescript-community/ui-chart";
 import { XAxisPosition } from "@nativescript-community/ui-chart/components/XAxis";
 import { BarData } from "@nativescript-community/ui-chart/data/BarData";
 import { BarDataSet } from "@nativescript-community/ui-chart/data/BarDataSet";
+import { CustomRenderer } from "@nativescript-community/ui-chart/renderer/AxisRenderer";
 import Vue from "nativescript-vue";
 
 export default Vue.extend({
   data() {
     return {
-      nowValues: [-18.17,-18.405,-18.5,-17.175,-16.66,-18.405,-9.275,0,0,0,0,0],
-      lastValues: [-18.45,-18.36,-17.685000000000002,-18.4,-18.275,-18.11,-16.875,-15.850000000000001,-17.535,-18.65,-18.16,-18.665]
+      nowValues: [0, 41, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0],
+      lastValues: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
   },
   computed: {
   },
   methods: {
     onChartLoaded() {
-      const chart = (this.$refs as any).chart['nativeView'] as BarChart;
+      const chart = (this as any).$refs.chart['nativeView'] as BarChart
       chart.drawGridBackgroundEnabled = false;
       chart.minOffset = 0;
       chart.drawBarShadowEnabled = false;
 
       const xAxis = chart.xAxis;
-      xAxis.drawGridLines = true;
+      xAxis.drawGridLines = false;
       xAxis.drawLimitLines = false;
       xAxis.drawAxisLine = false;
       xAxis.labelCount = 12;
       xAxis.position = XAxisPosition.BOTTOM;
       xAxis.valueFormatter = {
-        getAxisLabel: (value: number) => {
+        /**
+         * @param value {number}
+         */
+        getAxisLabel: (value) => {
           value = Math.ceil(value);
           if (value > 11 || value < 0) {
             return '';
           }
-          return (value * 2).toString()
+          return value.toString();
         }
       }
 
+      const yAxisCustomRenderer = {
+        drawZeroLine(c) {
+          const rect = chart.viewPortHandler.chartRect;
+          const pos = chart.rendererLeftYAxis.transformer.getPixelForValues(0, 0);
+          const paint = new Paint();
+          paint.setColor('#F2F2F2');
+          let y = pos.y - 2;
+          if (y < 0) {
+            y = 0;
+          }
+          const nrect = createRect(rect.left, y, rect.right - rect.left, 2);
+          c.drawRect(nrect, paint);
+        },
+      } satisfies CustomRenderer
+
       const rightAxis = chart.rightAxis;
       rightAxis.drawGridLines = false;
-      rightAxis.drawZeroLine = false;
+      rightAxis.drawZeroLine = true;
       rightAxis.drawAxisLine = false;
       rightAxis.drawLimitLines = false;
       rightAxis.drawLabels = false;
-      rightAxis.zeroLineWidth = 200;
-      rightAxis.zeroLineColor = '#F2F2F2'
+      rightAxis.customRenderer = yAxisCustomRenderer;
 
       const leftAxis = chart.leftAxis;
       leftAxis.drawGridLines = false;
-      leftAxis.drawZeroLine = false;
+      leftAxis.drawZeroLine = true;
       leftAxis.drawAxisLine = false;
       leftAxis.drawLimitLines = false;
       leftAxis.drawLabels = false;
-      rightAxis.zeroLineWidth = 200;
-      rightAxis.zeroLineColor = '#F2F2F2'
+      leftAxis.customRenderer = yAxisCustomRenderer;
 
       const nowData = this.nowValues.map((x, i) => ({
         index: i,
@@ -81,7 +98,7 @@ export default Vue.extend({
       lastSet.color = '#BEDAEF';
 
       const chartMax = Math.max(...nowData.map((v) => v.value), ...lastData.map((v) => v.value));
-      const chartMin = Math.min(...nowData.map((v) => v.value), ...lastData.map((v) => v.value));
+      const chartMin = Math.min(...nowData.map((v) => v.value), ...lastData.map((v) => v.value), 0);
       leftAxis.axisMaximum = chartMax;
       rightAxis.axisMaximum = chartMax;
       leftAxis.axisMinimum = chartMin;
@@ -89,33 +106,20 @@ export default Vue.extend({
 
       const bd = new BarData([lastSet, nowSet]);
       bd.barWidth = 0.025 * xAxis.labelCount;
-      bd.groupBars(0, 0.35, 0, true, true);
+      bd.groupBars(0, 1-0.05*xAxis.labelCount, 0, true, false);
       chart.data = bd;
-      const self = this;
+
       chart.customRenderer = {
         drawBar(c, e, dataSet, left, top, right, bottom, paint) {
           if (e.value > 0) {
             c.drawRoundRect(left, top, right, bottom, 4, 4, paint)
             c.drawRect(left, top + 4, right, bottom, paint)
-          } else {
+          } else if (e.value < 0) {
             c.drawRoundRect(left, top, right, bottom, 4, 4, paint)
             c.drawRect(left, top, right, bottom - 4, paint)
           }
-          self.drawZeroLine(chart, c);
         }
       }
-    },
-    drawZeroLine(chart: BarChart, canvas: Canvas) {
-      const rect = chart.viewPortHandler.chartRect;
-      const pos = chart.rendererLeftYAxis.transformer.getPixelForValues(0, 0);
-      const paint = new Paint();
-      paint.setColor('#F2F2F2');
-      let y = pos.y - 2;
-      if (y < 0) {
-        y = 0;
-      }
-      const nrect = createRect(rect.left, y, rect.right - rect.left, 2);
-      canvas.drawRect(nrect, paint);
     }
   }
 });
